222297409eb134e0cb770934fa5d7b06018e01fe
Mountain/\Ash <mountainash@rmwpublishing.net>
2019-06-13 09:51:22 +0200

    Design Commit
    The basic look is here - just need to make it function before beefing
    up with a framework (in another branch).

--- README.md
+++ README.md
@@ -5,5 +5,18 @@ A simple personal project to plan out my day to get some structure back in my li
 # Goals
 
 * Get on track (by getting off track making this...)
-* Learn more about Python/Django
+* Learn more GitHub
+* Learn more about Vue.js and related frameworks
 * Learn more about CD (using Netify)
+
+# TODO
+
+* highlight current task
+* pull timing from schedule.json
+* make resizable
+* add push notifications
+* PWA
+* OSX Notification Center Widget
+* add social+meta
+
+![Icon](apple-touch-icon.png)
---
+++ script.js
@@ -0,0 +1,18 @@
+// Taken from momentjs.com homepage
+function updateClock(){
+    var now = moment(),
+        second = now.seconds() * 6,
+        minute = now.minutes() * 6 + second / 60,
+        hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+
+    $('#hour').css("transform", "rotate(" + hour + "deg)");
+    $('#minute').css("transform", "rotate(" + minute + "deg)");
+    $('#second').css("transform", "rotate(" + second + "deg)");
+}
+
+function timedUpdate() {
+    updateClock();
+    setTimeout(timedUpdate, 1000);
+}
+
+timedUpdate();
